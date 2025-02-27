478485f66e6476d3738520efee39c97aae0b6903
InesSchoenmann <ines.schoenmann@gmail.com>
2025-02-27 19:45:34 +0100

    update model.py

--- lingpred_new/encoding_analysis.py
+++ lingpred_new/encoding_analysis.py
@@ -7,3 +7,7 @@ from sklearn.linear_model import Ridge
 from scipy import stats
 import spacy
 from sklearn.preprocessing import StandardScaler
+
+# make sure you add the path of the Lingpred folder to be able to import the gpt2 module:
+sys.path.append(os.path.abspath("/project/3018059.03/Lingpred"))
+import gpt2
